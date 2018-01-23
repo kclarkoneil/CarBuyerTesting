baec8da316541fc243abb0d39b2c16b7b73d92d3
Sam Meech-Ward <sam@meech-ward.com>
2018-01-23 14:22:16 -0800

    Add the should buy car function

---
+++ CarBuyer/Car.swift
@@ -0,0 +1,18 @@
+//
+//  Car.swift
+//  Car
+//
+//  Created by Sam Meech-Ward on 2018-01-23.
+//  Copyright © 2018 lighthouse-labs. All rights reserved.
+//
+
+import Foundation
+
+class Car {
+  
+  var type: String?
+  var color: String?
+  var price: Double?
+  var litresPer100km: Int?
+  
+}
---
+++ CarBuyer/ShouldBuyCar.swift
@@ -0,0 +1,39 @@
+//
+//  ShouldBuyCar.swift
+//  Car
+//
+//  Created by Sam Meech-Ward on 2018-01-22.
+//  Copyright © 2018 lighthouse-labs. All rights reserved.
+//
+
+import Foundation
+
+class ShouldBuyCar {
+  
+  func shouldBuy(car: Car) -> Bool {
+    if let type = car.type, type.lowercased() == "hatchback" {
+      return false
+    }
+    
+    if let color = car.color, color.lowercased() == "pink" {
+      return true
+    }
+    
+    guard let litresPer100km = car.litresPer100km else {
+      return false
+    }
+    guard let price = car.price else {
+      return false
+    }
+    
+    let goodLitres = litresPer100km >= 6 && litresPer100km <= 11
+    let goodPrice = price <= 5000
+    
+    if goodLitres && goodPrice {
+      return true
+    }
+    
+    return false
+  }
+  
+}
